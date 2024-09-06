9af83e19c183c5475b07bcfcaf05972ab9d07f71
Torben Frøberg <torfro@hofor.dk>
2024-09-06 20:29:32 +0200

    New test file

---
+++ test/test_data.py
@@ -0,0 +1,1 @@
+https://api.energidataservice.dk/dataset/ProductionConsumptionSettlement?offset=0&start=2024-08-01T00:00&end=2024-09-01T00:00&filter=%7B%22PriceArea%22:[%22DK2%22,%22%22]%7D&sort=HourUTC%20DESC
