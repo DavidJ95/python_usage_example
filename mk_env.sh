4ee7b1e87f0a99405faf70ae9fc92ad448f2eb44
Torben Frøberg <torfro@hofor.dk>
2024-09-06 12:50:56 +0200

    Changed mk_env.sh to work with default insatllation of python ob windows

--- mk_env.sh
+++ mk_env.sh
@@ -3,7 +3,7 @@ echo $1
 cd ./src
 if [ -d ".venv" ]; then rm -Rf .venv; fi
 echo "Danner env"
-${1} -m venv .venv
+"${1}" -m venv .venv
 source .venv/Scripts/activate
 cp ../pip.ini .venv
 echo "Installerer PyPi pakker"
